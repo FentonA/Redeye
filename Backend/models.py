39072ef606a6d77aa6008da2ceace616e1dd42c2
Alf Fenton <Fentonalf@gmail.com>
2020-05-26 08:17:53 -0400

    Feat: Implemented Database Models for Comments, and Posts.

--- Backend/models.py
+++ Backend/models.py
@@ -32,6 +32,7 @@ def setup_db(app, database_path=db_path ):
         # Authkey
         payment_id = db.Column(db.String(128), index=True)
         payment_history = db.Column(db.string(128), index=True)
+        comments = db.relationship('Comments', backref = 'cient', Lazy= True)
         
         
     class Artist(db.model):
@@ -41,18 +42,34 @@ def setup_db(app, database_path=db_path ):
         name = Column(String)
         #Authkey
         received_invoices = db.Column(db.String(128), index=True)
-        Gallery = db.relationship('Gallery', backref='artists', lazy=True)
+        gallery = db.relationship('Gallery', backref='artists', lazy=True)
+        comments = db.relationship('Comments', backref = 'artists', Lazy = True)
 
     class Gallery(db.model):
 
         id = Column(Integer, primary_key=True)
-        Title = Column(db.Varchar(120))
-        Description = Column(db.Varchar(255))
+        title = Column(db.Varchar(120))
+        description = Column(db.Varchar(255))
         photo = db.relationship('Photos', backref='Gallery', lazy=True)
     
     class Photos(db.model):
         __tablename__ = 'photos'
         id = Column(Integer, primary_key = True)
-        Title = Column(db.Vachar(120))
-        Description = Column(db.Varchar(128))
-        Image = Column(db.Varchar(50))
+        title = Column(db.Vachar(120))
+        description = Column(db.Varchar(128))
+        image = Column(db.Varchar(50))
+
+    class Posts(db.model):
+        __tablename__ = 'posts'
+        id = db.Column(db.Integer, primary_key=True)
+        description = db.Column(db.String(300))
+        price = db.Columnb(db.Varchar(10))
+        comments = db.relationship('Comments', backref ='Posts', lazy=True)
+
+    class Coments(db.model):
+        __tablename__ = 'comments'
+        id = db.Column(db.Integer, primary_key = True)
+        text = db.Column(db.String(128), index = True)
+        post_id = db.Column(db.Integer, db.ForeignKey('Posts.id'), nullable = False)
+        artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable = False)
+        client_id = db.Column(db.Integer, db.ForeignKey('Client.id'), nullable = False)
