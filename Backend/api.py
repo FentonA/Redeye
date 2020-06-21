d749fe76cda781903a871b70f6a084ba1a3a08a4
Alf Fenton <Fentonalf@gmail.com>
2020-06-21 12:16:45 -0400

    refactored necessary Imports

--- Backend/api.py
+++ Backend/api.py
@@ -4,20 +4,21 @@
 
 import json 
 import babel
-from flask import FLASK, render_template, request, Response, flash, redirect, url_for
-from flask_moment import flask_moment
-from flask_SQLAlchemy import flask_SQLAlchemy
+from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
-from flask_wtf import Formatter
-from form import *
-from flask_migrate import flask_migrate
+from flask_wtf import Form
+from flask_migrate import Migrate
 
 #-------------------------------------------------
 #Instance of a flask app
 #-------------------------------------------------
 
 app = Flask(__name__)
+moment = Moment(app)
+db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
