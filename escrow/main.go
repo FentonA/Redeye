ee2d43ba793cd26701e1acba108250570d724577
Alf Fenton <fentonalf@gmail.com>
2022-07-06 11:58:04 -0500

    shuffled around folder structure, downloaded gotenv

---
+++ escrow/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	// strip.key
+	// c, _ := customer.Get("cus")
+}
